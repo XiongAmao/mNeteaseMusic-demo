36b7bf9cd8b6a691d90d891ed6c7f4e4bff5b655
XiongAmao <onycat@live.cn>
2017-06-24 15:04:14 +0800

    播放按钮添加音乐未加载完样式

--- js/song.js
+++ js/song.js
@@ -2,12 +2,13 @@ $(function () {
   
   var jukebox = $('.jukebox-container')
   var audio = document.createElement('audio');
-  var discTurner = $('.disc-turner')
+  var discSwitch = $('.disc-switch')
   audio.src = '//ond8gcwbr.bkt.clouddn.com/%E6%B4%9B%E5%B0%98%E9%9E%85_vvv%20-%20%E6%9C%80%E5%90%8E%E4%B8%80%E9%A1%B5.mp3'
   console.dir(audio);
   $(audio).on('canplay',function(){
     audio.play()
     jukebox.addClass('playing')
+    discSwitch.removeClass('ready')
   })
   $(".js-play").on('click',function(){
     audio.play()
