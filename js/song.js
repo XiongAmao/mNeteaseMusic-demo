e74a59261600c36de8c2e0e879d1cdfe52529eeb
XiongAmao <onycat@live.cn>
2017-07-06 00:44:46 +0800

    添加歌词滚动

--- js/song.js
+++ js/song.js
@@ -1,28 +1,53 @@
 $(function () {
 
     let songId = location.search.match(/\bid=([^&]*)/)[1]
-    var jukebox = $('.jukebox-container')
-    var $discSwitch = $('.disc-switch')
-
+    let jukebox = $('.jukebox-container')
+    let $discSwitch = $('.disc-switch')
+    let $lyricScroll = $('.song-lyrics .lrc-scroll')
     var audio = document.createElement('audio');
 
     $.get("./json/song_list.json")
         .then(function (response) {
             response.forEach(function (element) {
                 if (element.id === songId) {
-                    getMusicInfo(element)
+
+                    setLyric(element)
+                    setMusicInfo(element)
+                    initPlayer(element.url)
+
                 }
             })
         })
 
-    function getMusicInfo(elm) {
-        setPlay(elm.url)
+    function setMusicInfo(elm) {
+
         $(".page .background").css("background-image", `url(${elm.coverBlurUrl})`)
         $(".song-cover").find('img').attr("src", elm.coverUrl)
         $(".js-song-name").text(elm.title)
         $('.js-song-author').text(elm.singer)
     }
-    function setPlay(url) {
+    function setLyric(elm) {
+        let array = elm.lyric.split('\n')
+        let regex = /^\[(.+)\](.+)$/
+        array = array.map(function (string) {
+            let match = string.match(regex)
+            if (match) {
+                return {
+                    time: match[1], words: match[2]
+                }
+            }
+        })
+        array.map(function (object) {
+            if (object) {
+                let $p = $('<p/>')
+                $p.attr('data-time', object.time)
+                    .text(object.words)
+                    .appendTo($lyricScroll)
+            }
+
+        })
+    }
+    function initPlayer(url) {
         audio.src = url
         $(audio).on('canplay', function () {
             audio.play()
@@ -39,6 +64,38 @@ $(function () {
             audio.pause()
             jukebox.removeClass('playing')
         })
+        setInterval(() => {
+            let audioCurrentTime = getAudioCurrentTime(audio)
+            let $p = $lyricScroll.find("p[data-time]")
+            let $whichLine
+            for (let i = 0; i < $p.length; i++) {
+                let currentLineTime = $p.eq(i).attr('data-time')
+                let nextLineTime = $p.eq(i + 1).attr('data-time')
+                if ($p.eq(i + 1).length !== 0 && currentLineTime < audioCurrentTime && nextLineTime > audioCurrentTime) {
+                    $whichLine = $p.eq(i)
+                    break
+                }
+            }
+            if ($whichLine) {
+                $whichLine.addClass("active").prev().removeClass('active')
+                let top = $whichLine.offset().top
+                let linesTop = $lyricScroll.offset().top
+                let delta = top - linesTop 
+                $lyricScroll.css('transform',`translateY(-${delta}px)`)
+            } 
+             
+
+        }, 200)
+    }
+    function getAudioCurrentTime(audio) {
+        let seconds = audio.currentTime
+        let munites = ~~(seconds / 60)
+        let left = seconds - munites * 60
+        let time = `${pad(munites)}:${pad(left)}`
+        return time
+    }
+    function pad(number) {
+        return number >= 10 ? number + "" : "0" + number
     }
 
 
