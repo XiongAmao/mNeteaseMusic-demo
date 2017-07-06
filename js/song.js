5cf8a918d2f8b4e6837a003e22e3b9262ba3cb9c
XiongAmao <onycat@live.cn>
2017-07-06 18:51:15 +0800

    添加 不同高度view下，歌词显示行数样式 & 调整vw样式

--- js/song.js
+++ js/song.js
@@ -1,26 +1,26 @@
+var audiotest
 $(function () {
 
     let songId = location.search.match(/\bid=([^&]*)/)[1]
     let jukebox = $('.jukebox-container')
     let $discSwitch = $('.disc-switch')
+    let $songLyrics = $(".song-lyrics")
     let $lyricScroll = $('.song-lyrics .lrc-scroll')
-    var audio = document.createElement('audio');
+
+    var audio = audiotest = document.createElement('audio');
 
     $.get("./json/song_list.json")
         .then(function (response) {
             response.forEach(function (element) {
                 if (element.id === songId) {
-
-                    setLyric(element)
                     setMusicInfo(element)
+                    setLyric(element)
                     initPlayer(element.url)
-
                 }
             })
         })
 
     function setMusicInfo(elm) {
-
         $(".page .background").css("background-image", `url(${elm.coverBlurUrl})`)
         $(".song-cover").find('img').attr("src", elm.coverUrl)
         $(".js-song-name").text(elm.title)
@@ -44,27 +44,41 @@ $(function () {
                     .text(object.words)
                     .appendTo($lyricScroll)
             }
-
         })
+        lyricResize()
     }
     function initPlayer(url) {
-        audio.src = url
-        $(audio).on('canplay', function () {
+        audio.src = "http://orzqfde1a.bkt.clouddn.com/test.mp3"
+        let lyricAnimationTimer
+        $(audio).on('canplay', () => {
             audio.play()
             console.log('can play now')
             jukebox.addClass('playing')
             $discSwitch.removeClass('ready')
             $('.js-icon-loading').remove()
+            lyricAnimationTimer = lyricAnimation()
         })
-        $(".js-play").on('click', function () {
+        $(".js-play").on('click', () => {
+            console.log("click play")
             audio.play()
+            lyricAnimationTimer = lyricAnimation()
             jukebox.addClass('playing')
         })
-        $(".js-pause").click(function () {
+        $(".js-pause").on("click", () => {
+            console.log("click pause")
             audio.pause()
             jukebox.removeClass('playing')
+            clearInterval(lyricAnimationTimer)
         })
-        setInterval(() => {
+        $(audio).on('ended', () => {
+            console.log("ended")
+            jukebox.removeClass('playing')
+            $('.lrc-scroll').css('transform', "")
+            clearInterval(lyricAnimationTimer)
+        })
+    }
+    function lyricAnimation() {
+        return setInterval(() => {
             let audioCurrentTime = getAudioCurrentTime(audio)
             let $p = $lyricScroll.find("p[data-time]")
             let $whichLine
@@ -80,12 +94,27 @@ $(function () {
                 $whichLine.addClass("active").prev().removeClass('active')
                 let top = $whichLine.offset().top
                 let linesTop = $lyricScroll.offset().top
-                let delta = top - linesTop 
-                $lyricScroll.css('transform',`translateY(-${delta}px)`)
-            } 
-             
-
+                let delta = top - linesTop
+                $lyricScroll.css('transform', `translateY(-${delta}px)`)
+            }
         }, 500)
+    }
+    function lyricResize() {
+        let slTop = $songLyrics.offset().top
+        let height = $songLyrics.height()
+        let slBottom = slTop + height
+        let lTop = $('.links').offset().top
+        if (lTop < slBottom + 20) {
+            if (lTop - slTop > height / 3 * 2) {
+                $songLyrics.css('height', `${height / 3 * 2}px`)
+                    .attr('data-lines', 2)
+            } else if (lTop - slTop < height / 3 * 2 + 10) {
+                $songLyrics.css('height', `${height / 3}px`)
+                    .attr('data-lines', 1)
+            } else {
+                return
+            }
+        }
     }
     function getAudioCurrentTime(audio) {
         let seconds = audio.currentTime
@@ -97,6 +126,4 @@ $(function () {
     function pad(number) {
         return number >= 10 ? number + "" : "0" + number
     }
-
-
 })
